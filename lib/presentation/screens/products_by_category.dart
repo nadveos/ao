e499208dc61d6c9272ea61de187c83c3e717daed
Milonguita <guta.flores@gmail.com>
2025-05-09 13:44:43 -0300

    change title position

--- lib/presentation/screens/products_by_category.dart
+++ lib/presentation/screens/products_by_category.dart
@@ -24,6 +24,7 @@ class _ProductsByCategoryPageState extends ConsumerState<ProductsByCategoryPage>
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
+        centerTitle: true,
         actions: [
           Stack(
             alignment: Alignment.center,
